bbfc1b229a4cd5cef30a40554a048c62e7ee2daa
Dmitri Mendis <2dmendis@gmail.com>
2018-06-01 18:54:14 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,45 @@
+class Artist 
+  attr_accessor :name 
+  @@all = []
+  #@song = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(name)
+    @name = name 
+    @song = []
+  end 
+
+  def add_song(song)
+    @song << song
+    song.artist = self 
+  end 
+  
+  def songs 
+    @song
+  end 
+  
+  def save
+    @@all << self 
+  end 
+  
+  def self.find_or_create_by_name(name) 
+    if self.all.detect{|person| person.name == name}
+      person = self.all.detect{|person| person.name == name}
+    else 
+      person = self.new(name)
+      person.name = name
+      @@all << person
+    end 
+    person 
+  end 
+  
+  def print_songs
+    @song.each do |i|
+      puts i.name 
+    end 
+  end 
+  
+end 
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,21 @@
+
+class MP3Importer 
+  attr_accessor :path 
+  
+  def initialize(path) 
+    #@path = Dir.entries(path)
+    @path = path
+  end 
+  
+  def files
+    Dir.entries(@path).select{|file| file != "." && file != ".."}
+    
+  end 
+  
+  def import
+    files.each do |i|
+      x = Song.new_by_filename(i)
+      x
+    end 
+  end 
+end 
